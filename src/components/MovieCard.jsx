088708c2ac17591e1ce7e82548f542e382d597d9
mariananogueirab <mariananbp@gmail.com>
2021-07-22 19:04:58 -0300

    Requisitos finais e css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Rating from './Rating';
+import '../App.css';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     const { imagePath, title, subtitle, storyline, rating } = movie;
     return (
-      <div>
-        <img src={ imagePath } alt={ `Foto do ${title}` } />
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
-        <Rating rating={ rating } />
+      <div className="movie-card">
+        <img src={ imagePath } alt={ `Foto do ${title}` } className="movie-card-image" />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+        </div>
+        <div className="movie-card-rating">
+          <Rating rating={ rating } />
+        </div>
       </div>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../App.css';
 
 class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <div>
+      <div className="rating">
         {rating}
       </div>
     );
@@ -13,7 +14,7 @@ class Rating extends React.Component {
 }
 
 Rating.propTypes = {
-  rating: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
 };
 
 export default Rating;
